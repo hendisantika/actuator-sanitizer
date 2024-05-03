d896a87517fcd6c36c9017785d18f82a841f1642
hendisantika <hendisantika@yahoo.co.id>
2024-05-04 06:51:42 +0700

    Create Spring Boot project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/actuatorsanitizer/ActuatorSanitizerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.actuatorsanitizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorSanitizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorSanitizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/actuatorsanitizer/ActuatorSanitizerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.actuatorsanitizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorSanitizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
